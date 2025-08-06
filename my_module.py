eecd8df5c42ef44c240c1dcb76ac458096960a87
Ozuzu Angel Edwin <ozuzuedwin5@gmail.com>
2025-08-05 17:39:49 -0700

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,10 @@
+def count_words(text):
+    """Returns the number of words in the input text"""
+    return len(text.split())
+
+def count_characters(text):
+    """Returns the number of characters in the input text"""
+    return len(text)
+
+
+"Add my_module.py with two functions"
